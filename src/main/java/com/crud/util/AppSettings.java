75d544abdcaa40ba6bd2e4aa438b4683b111760f
Impalatore <naim7710@gmail.com>
2022-08-01 12:15:43 -0500

    Correciones

--- src/main/java/com/crud/util/AppSettings.java
+++ src/main/java/com/crud/util/AppSettings.java
@@ -2,6 +2,6 @@ package com.crud.util;
 
 public class AppSettings {
 
-	public static final String URL_CROSS_ORIGIN = "http://localhost:4200";
+	public static final String URL_CROSS_ORIGIN = "http://localhost:3000";
 	
 }
